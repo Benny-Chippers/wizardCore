c3107d2f736539da2e6d387718f2f114e45f7c27
ReverseBack2 <donovan.burk@gmail.com>
2026-06-02 00:52:59 -0700

    determined script was wrong

--- src/testBench.sv
+++ src/testBench.sv
@@ -2,67 +2,70 @@
 timeunit 1ns;
 timeprecision 1ns;
 `else
-`timescale 1ns/1ns;
+`timescale 1ns/1ns
 `endif
 
+module testBench;
+    logic osc_clk;
+    logic hit_reset;
+    logic reset_n;
 
-module testBench(
     `ifdef SIMULATION
-    output [13:0] vgaData,
-    `else
-    input osc_clk,
-    output macro_pkg::vga_out_t vgaData,
+    logic clk;      // System/CPU clock
+    logic vga_clk;  // VGA clock
+    logic spi_clk;  // SPI clock
     `endif
-    inout [5:0] spi
-    // inout [31:0] gpio
-);
-    reg osc_clk;
-    reg clk;      // System/CPU Clock
-    reg vga_clk;  // Clock for vga circuit
-    reg spi_clk;
 
-    reg hit_reset;
-    reg reset_n;
-    
-    reg MISO;
-    reg D_RDY;
-    
+    macro_pkg::vga_out_t vgaData;
+
+    wire [5:0] spi;
+    wire [31:0] gpio_bus;
+
+    logic MISO;
+    logic D_RDY;
+    logic [31:0] gpio_drive;
+    logic [31:0] gpio_drive_en;
+
     assign spi[1] = MISO;
     assign spi[3] = D_RDY;
 
-    initial
-     begin
+    initial begin
         `ifdef SIMULATION
-        clk = 0;
-        vga_clk = 0;
-        spi_clk = 0;
+        clk = 1'b0;
+        vga_clk = 1'b0;
+        spi_clk = 1'b0;
         `endif
-        osc_clk = 0;
-        reset_n = 0;
-        hit_reset = 1;
-        #15us hit_reset = 0;
-        // #50ns hit_reset = 0;
-     end
 
-    // Simulation clocking for Verilator
+        osc_clk = 1'b0;
+        MISO = 1'b0;
+        D_RDY = 1'b0;
+        reset_n = 1'b0;
+        hit_reset = 1'b1;
+
+        // test_special_regs.mem expects external GPIO pin 8 to be driven high.
+        gpio_drive = 32'h0000_0100;
+        gpio_drive_en = 32'h0000_0100;
+
+        #15us hit_reset = 1'b0;
+    end
+
     `ifdef SIMULATION
     always begin
         #10ns clk <= ~clk;
-     end
+    end
 
-     always begin
-         #20ns vga_clk <= ~vga_clk;
-     end
+    always begin
+        #20ns vga_clk <= ~vga_clk;
+    end
 
-     always begin
-         #500ns spi_clk <= ~spi_clk;
-     end
-     `else
-     always begin
+    always begin
+        #500ns spi_clk <= ~spi_clk;
+    end
+    `else
+    always begin
         #10ns osc_clk <= ~osc_clk;
-     end
+    end
     `endif
-
 
     `ifdef SIMULATION
     always @(posedge clk)
@@ -70,51 +73,30 @@ module testBench(
     always @(posedge osc_clk)
     `endif
     begin
-        if(hit_reset === 1'b1) begin
-            reset_n <= 0;
-        end
-        else begin
-            reset_n <= 1;
-        end
+        reset_n <= ~hit_reset;
     end
-
-
-    initial
-     begin
-         $display("[%0t] Tracing to dump.vcd...\n",$time);
-         $dumpfile("dump.vcd");
-         $dumpvars();
-         $display("[%0t] Model running...\n",$time);
-         `ifdef SIMULATION
-         $display("[%0t] Simulation mode enabled.\n",$time);
-         `endif
-     end
-
-    initial
-     begin
-          // #50us $dumpflush;
-        #50ms $dumpflush;
-         $finish;
-     end
-
-     // initial begin
-
-     // end
-
-     reg [31:0] gpio;
 
     initial begin
-        gpio = 32'h0000_0100;   /* bit 8 high for test_gpio_input_pin8() */
+        $display("[%0t] Tracing to dump.vcd...\n", $time);
+        $dumpfile("dump.vcd");
+        $dumpvars();
+        $display("[%0t] Model running...\n", $time);
+        `ifdef SIMULATION
+        $display("[%0t] Simulation mode enabled.\n", $time);
+        `endif
     end
 
-    wire [31:0] gpio_bus;
-    wire [31:0] gpio_dir = top_instance.SPECIAL.GPIO.r_direction;
-    wire [31:0] gpio_out = top_instance.SPECIAL.GPIO.r_out_reg;
+    initial begin
+        #10ms $dumpflush;
+        $finish;
+    end
 
     genvar gi;
     generate
-        for (gi = 0; gi < 32; gi = gi + 1) begin : g_gpio_pin_hookup
-            assign gpio_bus[gi] = gpio_dir[gi] ? gpio_out[gi] : gpio[gi];
+        for (gi = 0; gi < 32; gi = gi + 1) begin : g_gpio_tb_drive
+            assign gpio_bus[gi] =
+                (gpio_drive_en[gi] && !top_instance.SPECIAL.GPIO.r_direction[gi]) ?
+                gpio_drive[gi] : 1'bz;
         end
     endgenerate
 
@@ -131,7 +113,5 @@ module testBench(
         .spi        (spi),
         .gpio       (gpio_bus)
     );
-
-
 
 endmodule
